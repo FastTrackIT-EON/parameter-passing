1d81868f2b6720faba7acee482f272eabf900a14
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-05-08 09:33:59 +0300

    initial project structure

---
+++ ParameterPassing/ParameterPassing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterPassing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine($"Before Calling Increment: {i}");
+            Increment(i);
+            Console.WriteLine($"After Calling Increment: {i}");
+        }
+
+        private static void Increment(int i)
+        {
+            Console.WriteLine($"Before increment: {i}");
+            i = i + 1;
+            Console.WriteLine($"After increment: {i}");
+        }
+    }
+}
